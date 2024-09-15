6764832213ee7168140f930b9b67f84e1b1fd6d1
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-09-15 15:25:51 +0200

    add favicon

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f95de7936c680942")
+/******/ 		__webpack_require__.h = () => ("13da6f50c14aaa8f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "111a1db9a1b21157"; }
+/******/ 		__webpack_require__.h = function() { return "19939628bfee41b0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
